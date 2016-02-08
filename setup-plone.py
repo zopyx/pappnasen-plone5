5473790195f3367d1b9053500ec82d1992c45574
Andreas Jung <info@zopyx.com>
2016-02-08 14:48:10 +0100

    updated

--- setup-plone.py
+++ setup-plone.py
@@ -16,7 +16,7 @@ newSecurityManager(None, user.__of__(uf))
 
 if 'plone' in app.objectIds():
     app.manage_delObjects(['plone'])
-addPloneSite(app, 'plone', extension_ids=['plonetheme.barceloneta:default', 'zopyx.ipsumplone:default', 'Products.PloneFormGen:default', 'plone.app.multilingual:default'])
+addPloneSite(app, 'plone', extension_ids=['plonetheme.barceloneta:default', 'zopyx.ipsumplone:default', 'Products.PloneFormGen:default', 'plone.app.multilingual:default', 'plone.app.multilingual:default', 'Products.PloneFormGen:default'])
 
 plone.api.user.create(username='editor', password='editor', roles=('Member', 'Editor'), email="test@test.de")
 plone.api.user.create(username='reader', password='reader', roles=('Member', 'Reader'), email="test@test.de")
